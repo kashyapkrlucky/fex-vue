0bc9bd8edddaa3624745e40044253116ea8cf61c
Lucky Kashyap <kashyapkrlucky@gmail.com>
2025-07-03 00:57:17 +0200

    layout changes

--- src/components/layout/PageWrapper.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="max-w-5xl mx-auto w-full space-y-4">
-    <slot />
-  </div>
-</template>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,34 @@
-<script setup lang="ts">
-import AppLayout from '@/components/layout/AppLayout.vue'
-import PageWrapper from '@/components/layout/PageWrapper.vue'
-</script>
-
 <template>
-  <AppLayout>
-    <PageWrapper>
-      <router-view />
-    </PageWrapper>
-  </AppLayout>
+  <div class="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
+    <header
+      class="flex items-center justify-between h-14 px-4 border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800">
+      <h1 class="text-lg font-semibold">App Name</h1>
+      <div class="flex items-center space-x-4">
+        <LanguageSwitcher></LanguageSwitcher>
+        <UserMenu></UserMenu>
+      </div>
+    </header>
+    <div class="flex flex-row flex-1">
+      <aside
+        class="transition-colors duration-300 w-64 hidden md:block bg-white dark:bg-gray-900 text-black dark:text-white border-r border-gray-200 dark:border-gray-700">
+        <nav class="p-4 space-y-2">
+          <RouterLink to="/" class="block px-3 py-2 rounded hover:bg-gray-100 dark:hover:bg-gray-700"
+            active-class="bg-gray-100 dark:bg-gray-700 font-medium">
+            {{ $t('nav.home') }}
+          </RouterLink>
+          <RouterLink to="/about" class="block px-3 py-2 rounded hover:bg-gray-100 dark:hover:bg-gray-700"
+            active-class="bg-gray-100 dark:bg-gray-700 font-medium">
+            {{ $t('nav.about') }}
+          </RouterLink>
+        </nav>
+      </aside>
+      <main class="flex-1 overflow-y-auto p-4">
+        <router-view />
+      </main>
+    </div>
+  </div>
 </template>
+<script setup lang="ts">
+import UserMenu from "@/components/base/UserMenu.vue";
+import LanguageSwitcher from "@/components/base/LanguageSwitcher.vue";
+</script>
